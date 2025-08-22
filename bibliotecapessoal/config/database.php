46e7dc457f27b4eff84d89791cd17b6149a61dd7
lyandraalmeidas <lyandraalmeidadasilva99@gmail.com>
2025-08-21 21:04:50 -0300

    tabelas

--- bibliotecapessoal/config/database.php
+++ bibliotecapessoal/config/database.php
@@ -1,7 +1,9 @@
 <?php
+// filepath: /c:/xampp/htdocs/Biblioteca-Pessoal/bibliotecapessoal/config/database.php
+<?php
 return [
     'host' => 'localhost',
-    'dbname' => 'biblioteca_pessoal',
+    'dbname' => 'biblioteca_pessoal', // Nome do banco importado
     'user' => 'root',
     'password' => '',
     'charset' => 'utf8mb4',
